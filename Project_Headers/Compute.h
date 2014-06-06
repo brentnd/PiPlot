7ce2ff7e74e41b0e68e01b7590ffe7490a04ee28
brentnd <brentdimmig@gmail.com>
2014-06-05 22:04:33 -0400

    Added compute module for calculating positions to motors.

---
+++ Project_Headers/Compute.h
@@ -0,0 +1,7 @@
+#ifndef COMPUTE_H_
+#define COMPUTE_H_
+
+#include <math.h>
+
+
+#endif /* COMPUTE_H_ */
---
+++ Sources/Compute.c
@@ -0,0 +1,10 @@
+#include "Compute.h"
+
+void MovementGoto(int x, int y)
+{
+  // Unimplemented
+  // Translate X, Y coords into lengths 
+  // for each stepper motor.
+  
+  // Update stepper motors to those lengths
+}
